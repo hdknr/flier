9194124d2f020c1cf3d7e1bc8a7d4e240629a512
hdknr <gmail@hdknr.com>
2015-10-09 01:59:14 +0000

    restart

--- lib/flier/admin.py
+++ lib/flier/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
-from django.db.models import get_app, get_models
+from django.apps import apps
 
 
-for model in get_models(get_app(__name__.split('.')[-2:][0])):
+def register(app_fullname, admins, ignore_models=[]):
+    app_label = app_fullname.split('.')[-2:][0]
+    for n, model in apps.get_app_config(app_label).models.items():
+        if model.__name__ in ignore_models:
+            continue
+        name = "%sAdmin" % model.__name__
+        admin_class = admins.get(name, None)
+        if admin_class is None:
+            admin_class = type(
+                "%sAdmin" % model.__name__,
+                (admin.ModelAdmin,), {},
+            )
 
-    admin_class = type(
-        "%sAdmin" % model.__name__,
-        (admin.ModelAdmin,),
-        dict(list_display=tuple([f.name for f in model._meta.fields]),)
-    )
+        if admin_class.list_display == ('__str__',):
+            excludes = getattr(admin_class, 'list_excludes', ())
+            additionals = getattr(admin_class, 'list_additionals', ())
+            admin_class.list_display = tuple(
+                [f.name for f in model._meta.fields
+                 if f.name not in excludes]) + additionals
 
-    admin.site.register(model, admin_class)
+        admin.site.register(model, admin_class)
 
+
+register(__name__, globals())
