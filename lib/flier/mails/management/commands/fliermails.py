768f76a061fd0f5a6ee9290ae1ee97a29956b86e
hdknr <gmail@hdknr.com>
2016-08-27 02:50:30 +0000

    ls_mails: specific id(s)

--- lib/flier/mails/management/commands/fliermails.py
+++ lib/flier/mails/management/commands/fliermails.py
@@ -19,10 +19,14 @@ def main(ctx):
 
 @main.command()
 @click.option('--queueing', '-q', is_flag=True)
+@click.option('--id', '-i', multiple=True)
 @click.pass_context
-def ls_mails(ctx, queueing):
+def ls_mails(ctx, queueing, id):
     ''' list all Mail'''
     mails = queueing and Mail.objects.queueing_set() or Mail.objects.all()
+    if id:
+        mails = mails.filter(id__in=id)
+
     for mail in mails:
         echo(u"{{ mail.id }}\t\
 {{ opt.app_label }}.{{ opt.object_name }}\t\
